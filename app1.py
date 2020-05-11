2bd70638a817161535eddf9083ae9091255745f4
sookbun <blackdew7@gmail.com>
2020-05-11 13:42:27 +0900

    get_template function

--- app1.py
+++ app1.py
@@ -4,6 +4,11 @@ app = Flask(__name__, static_folder="static")
 app.env = 'development'
 app.debug = True
 
+def get_template():
+    with open('./web/template.html', 'r', encoding='utf8') as f:
+        content = f.read()
+    return content
+
 @app.route('/')
 def main():
     f = open('./web/index.html', 'r', encoding='utf8')
@@ -13,15 +18,13 @@ def main():
 
 @app.route('/html')
 def html():
-    with open('./web/template.html', 'r', encoding='utf8') as f:
-        content = f.read()
-    return content.format('HTML', "HTML is ...")
+    temlate = get_template()
+    return temlate.format('HTML', "HTML is ...")
 
 @app.route('/css')
 def css():
-    with open('./web/template.html', 'r', encoding='utf8') as f:
-        content = f.read()
-    return content.format('CSS', "CSS is ...")
+    temlate = get_template()
+    return temlate.format('CSS', "CSS is ...")
 
 
 app.run(port=5001)
