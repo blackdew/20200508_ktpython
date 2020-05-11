dbf18aaa01853c1f83f24093b287fc3e80cbb840
sookbun <blackdew7@gmail.com>
2020-05-11 13:48:15 +0900

    template 개선

--- app1.py
+++ app1.py
@@ -4,27 +4,25 @@ app = Flask(__name__, static_folder="static")
 app.env = 'development'
 app.debug = True
 
-def get_template():
-    with open('./web/template.html', 'r', encoding='utf8') as f:
+def get_template(filename):
+    with open(f'./web/{filename}', 'r', encoding='utf8') as f:
         content = f.read()
     return content
 
 @app.route('/')
 def main():
-    f = open('./web/index.html', 'r', encoding='utf8')
-    content = f.read()
-    f.close()
-    return content
+    template = get_template('index.html')
+    return template
 
 @app.route('/html')
 def html():
-    temlate = get_template()
-    return temlate.format('HTML', "HTML is ...")
+    template = get_template('template.html')
+    return template.format('HTML', "HTML is ...")
 
 @app.route('/css')
 def css():
-    temlate = get_template()
-    return temlate.format('CSS', "CSS is ...")
+    template = get_template('template.html')
+    return template.format('CSS', "CSS is ...")
 
 
 app.run(port=5001)
