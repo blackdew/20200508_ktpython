9d367069a1c2083b49906ef2fcad2a80a63135f3
sookbun <blackdew7@gmail.com>
2020-05-12 09:18:32 +0900

    use querystring

--- day3.py
+++ day3.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 app.env = 'development'
@@ -12,9 +12,19 @@ def index():
 # *로 N줄의 트리를 만듭니다.
 @app.route('/tree/<num>')
 def tree(num):
+    if not num.isnumeric():
+        return "not number"
+
+    # request.form 어제 배운거 html form으로 부터 받을 때,
+    print(request.args.get('order'))
+
     trees = []
-    for i in range(int(num)):
-        trees.append("*" * (i + 1))
+    if request.args.get('order') == 'desc':
+        for i in range(int(num)):
+            trees.append("*" * (int(num) - i))
+    else:
+        for i in range(int(num)):
+            trees.append("*" * (i + 1))
 
     return '<br>'.join(trees)
 
