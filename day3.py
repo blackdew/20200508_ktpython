d46fcd8831c36c551b559347067a3f7576000a62
sookbun <blackdew7@gmail.com>
2020-05-12 09:12:17 +0900

    add tree

--- day3.py
+++ day3.py
@@ -8,4 +8,14 @@ app.debug = True
 def index():
     return "Welcome, Day 3 class"
 
+# 사용자로부터 숫자를 N을 입력 받아서,
+# *로 N줄의 트리를 만듭니다.
+@app.route('/tree/<num>')
+def tree(num):
+    trees = []
+    for i in range(int(num)):
+        trees.append("*" * (i + 1))
+
+    return '<br>'.join(trees)
+
 app.run()
