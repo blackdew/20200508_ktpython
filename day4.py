5ce65916ce15866c207c9197be5764100f048508
sookbun <blackdew7@gmail.com>
2020-05-13 09:07:48 +0900

    templates - 1

--- day4.py
+++ day4.py
@@ -1,11 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates')
 app.env = 'development'
 app.debug = True
 
 @app.route('/')
 def index():
-    return "Welcome, class day 4"
+    # return "Welcome, class day 4"
+    return render_template('index.html')
 
 app.run()
