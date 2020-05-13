f5a0e25aa9bfa67c0e66a5d9bb3625461e17801c
sookbun <blackdew7@gmail.com>
2020-05-13 09:39:50 +0900

    word count 3 - POST에 대한 route method 설정

--- day4.py
+++ day4.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__, template_folder='templates')
 app.env = 'development'
@@ -9,8 +9,12 @@ def index():
     # return "Welcome, class day 4"
     return render_template('index.html', message="my message")
 
-@app.route('/wordcount')
+@app.route('/wordcount', methods=['get', 'post'])
 def wordcount():
-    return render_template('word_count.html')
+    words = ''
+    if request.method == 'POST':
+        words = request.form.get('lyrics')
+
+    return render_template('word_count.html', words=words)
 
 app.run()
