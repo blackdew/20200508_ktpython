ff14bd9f38bae64ff4c1266c11ccf8512fee377c
sookbun <blackdew7@gmail.com>
2020-05-13 13:04:50 +0900

    word count 한글 1 - wordcount 함수 개선

--- day4.py
+++ day4.py
@@ -9,21 +9,25 @@ def index():
     # return "Welcome, class day 4"
     return render_template('index.html', message="my message")
 
-@app.route('/wordcount', methods=['get', 'post'])
-def wordcount():
+@app.route('/wordcount/<lang>', methods=['get', 'post'])
+def wordcount(lang):
     words = []
     if request.method == 'POST':
-        # 문자열 전처리
-        words = request.form.get('lyrics').strip().lower()
-        words = words.replace('\n', ' ')
-        specials = set(words) - set('abcdefghijklmnopqrstuvwxyz ')
-        for s in specials:
-            words = words.replace(s, '')
+        if lang == 'en':
+            # 문자열 전처리
+            words = request.form.get('lyrics').strip().lower()
+            words = words.replace('\n', ' ')
+            specials = set(words) - set('abcdefghijklmnopqrstuvwxyz ')
+            for s in specials:
+                words = words.replace(s, '')
 
-        # 단어 카운트
-        words = words.split(' ')
-        words = [(w, words.count(w)) for w in set(words)]
-        words = sorted(words, key=lambda x: x[1], reverse=True)
+            # 단어 카운트
+            words = words.split(' ')
+            words = [(w, words.count(w)) for w in set(words)]
+            words = sorted(words, key=lambda x: x[1], reverse=True)
+        else:
+            # 한글의 경우 단어카운트
+            words = request.form.get('lyrics').strip()
 
     return render_template('word_count.html', words=words)
 
